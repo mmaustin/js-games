5d55c6d0540777af76475d0fd1c9ca8649309d1e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-08-23 11:10:48 -0400

    added code for a triangle and smiley face to mdn script file

--- mdnScript.js
+++ mdnScript.js
@@ -3,11 +3,29 @@ function draw() {
     if (canvas.getContext) {
       const ctx = canvas.getContext('2d');
 
-      ctx.fillStyle = 'rgb(200, 0, 0)';
-      ctx.fillRect(10, 10, 50, 50);
+      //rectangle
+      /*ctx.fillRect(25, 25, 100, 100);
+      ctx.clearRect(45, 45, 60, 60);
+      ctx.strokeRect(50, 50, 50, 50);*/
 
-      ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-      ctx.fillRect(30, 30, 50, 50);
+      //triange
+      /*ctx.beginPath();
+      ctx.moveTo(75, 50);
+      ctx.lineTo(100, 75);
+      ctx.lineTo(100, 25);
+      ctx.fill();*/
+      
+      //smiley face
+      ctx.beginPath();
+      ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+      ctx.moveTo(110, 75);
+      ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+      ctx.moveTo(65, 65);
+      ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+      ctx.moveTo(95, 65);
+      ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+      ctx.stroke();      
+
     }
   }
   draw();
