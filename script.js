46528e84dcdba4980553fbcd6a37082e7b4a86de
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-08-24 13:01:42 -0400

    added proper image to canvas tutorial

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
-playerImage.src = 'https://images.unsplash.com/photo-1624943233648-9cf5cc4ee471?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80';
-const snipWidth = 75;
-const snipHeight = 70;
+//playerImage.src = 'https://images.unsplash.com/photo-1624943233648-9cf5cc4ee471?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80';
+playerImage.src = 'https://www.frankslaboratory.co.uk/downloads/shadow_dog.png';
+const snipWidth = 575;
+const snipHeight = 523;
 let frameX = 0;
 let frameY = 0;
 let gameFrame = 0;
@@ -15,11 +16,13 @@ const staggerFrames =5;
 
 function animate(){
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    let position = Math.floor(gameFrame/staggerFrames) % 6;
+    frameX = snipWidth * position;
     //ctx.fillRect(100,50,100,100);
     //draw image method can take up to nine arguments.
     //ctx.drawImage(image, sourcex, sy, sourceWidth, sourceHeight, and the last four arguments from the function below)
-    ctx.drawImage(playerImage, frameX * snipWidth, frameY * snipHeight,
-         snipWidth, snipHeight, 0, 0, snipWidth, snipHeight);
+    ctx.drawImage(playerImage, frameX , frameY * snipHeight,
+    snipWidth, snipHeight, 0, 0, snipWidth, snipHeight);
 
     gameFrame++;
     //requestAnimationFrame continuously runs animate
