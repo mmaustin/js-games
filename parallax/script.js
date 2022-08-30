bf75019de6eafb97746c90af5730fb4644d85e2d
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-08-30 15:36:23 -0400

    added files and folders for project game three

--- parallax/script.js
+++ parallax/script.js
@@ -15,52 +15,55 @@ backgroundLayer4.src = '../images/layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = '../images/layer-5.png';
 
-const slider = document.getElementById('slider');
-slider.value = gameSpeed;
-const showGameSpeed = document.getElementById('showGameSpeed');
-showGameSpeed.innerHTML = gameSpeed;
-slider.addEventListener('change', (e)=>{
-    gameSpeed = e.target.value;
-    showGameSpeed.innerHTML = e.target.value;
+window.addEventListener('load', function(){
+    const slider = document.getElementById('slider');
+    slider.value = gameSpeed;
+    const showGameSpeed = document.getElementById('showGameSpeed');
+    showGameSpeed.innerHTML = gameSpeed;
+    slider.addEventListener('change', (e)=>{
+        gameSpeed = e.target.value;
+        showGameSpeed.innerHTML = e.target.value;
+    })
+
+    class Layer {
+        constructor(image, speedModifier){
+            this.x = 0;
+            this.y = 0;
+            this.width = 2400;
+            this.height = 700;
+            this.image = image;
+            this.speedModifier = speedModifier;
+            this.speed = gameSpeed * this.speedModifier;
+        }
+        update = () => {
+            this.speed = gameSpeed * this.speedModifier;
+            if(this.x <= -this.width){
+                this.x = 0;
+            }
+            this.x = Math.floor(this.x - this.speed);
+        }
+        draw = () => {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+        }
+    }
+
+    const layer1 = new Layer(backgroundLayer1, 0.2);
+    const layer2 = new Layer(backgroundLayer2, 0.4);
+    const layer3 = new Layer(backgroundLayer3, 0.6);
+    const layer4 = new Layer(backgroundLayer4, 0.8);
+    const layer5 = new Layer(backgroundLayer5, 1.0);
+
+    const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+
+    function animate(){
+        ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);    
+        gameObjects.map(object => {
+            object.update();
+            object.draw();
+        })
+        requestAnimationFrame(animate);
+    }
+    animate();
 })
 
-class Layer {
-    constructor(image, speedModifier){
-        this.x = 0;
-        this.y = 0;
-        this.width = 2400;
-        this.height = 700;
-        this.image = image;
-        this.speedModifier = speedModifier;
-        this.speed = gameSpeed * this.speedModifier;
-    }
-    update = () => {
-        this.speed = gameSpeed * this.speedModifier;
-        if(this.x <= -this.width){
-            this.x = 0;
-        }
-        this.x = Math.floor(this.x - this.speed);
-    }
-    draw = () => {
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
-    }
-}
-
-const layer1 = new Layer(backgroundLayer1, 0.2);
-const layer2 = new Layer(backgroundLayer2, 0.4);
-const layer3 = new Layer(backgroundLayer3, 0.6);
-const layer4 = new Layer(backgroundLayer4, 0.8);
-const layer5 = new Layer(backgroundLayer5, 1.0);
-
-const gameObjects = [layer1, layer2, layer3, layer4, layer5];
-
-function animate(){
-    ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);    
-    gameObjects.map(object => {
-        object.update();
-        object.draw();
-    })
-    requestAnimationFrame(animate);
-}
-animate();
