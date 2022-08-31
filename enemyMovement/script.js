09de2705dbd4a6e8d55b75433435d7b86ca75109
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-08-31 12:06:55 -0400

    placed each new enemy in an array upon construction

--- enemyMovement/script.js
+++ enemyMovement/script.js
@@ -3,13 +3,16 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
+numberOfEnemies = 10;
+ENEMIES_ARRAY = [];
 
 class Enemy{
     constructor(){
-        this.x = 10;
-        this.y = 50;
+        this.x = Math.random() * CANVAS_WIDTH;
+        this.y = Math.random() * CANVAS_HEIGHT;
         this.width = 100;
         this.height = 100;
+        ENEMIES_ARRAY.push(this);
     }
 
     update() {
@@ -23,7 +26,10 @@ class Enemy{
 }
 
 const enemy1 = new Enemy();
-
+for(let i = 0; i < numberOfEnemies; i++){
+    new Enemy();
+}
+console.log(ENEMIES_ARRAY)
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     enemy1.update();
