e8270e413304aeb909bfc30bbf956e22b43e72f4
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-08-31 11:26:33 -0400

    added an Enemy class

--- enemyMovement/script.js
+++ enemyMovement/script.js
@@ -4,12 +4,16 @@ const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
 
-enemy1 = {
-    x: 0,
-    y: 0,
-    width: 200,
-    height: 200
+class Enemy{
+    constructor(){
+        this.x = 10;
+        this.y = 50;
+        this.width = 100;
+        this.height = 100;
+    }
 }
+
+const enemy1 = new Enemy();
 
 function animate(){
     ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
