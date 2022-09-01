6dc3d9ac3771de297934b377e9a9ffcc3f885b18
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-01 10:56:59 -0400

    animated the enemy sprites

--- enemyMovement/script.js
+++ enemyMovement/script.js
@@ -13,22 +13,24 @@ class Enemy{
     constructor(){
         this.x = Math.random() * CANVAS_WIDTH;
         this.y = Math.random() * CANVAS_HEIGHT;
-        this.width = 100;
-        this.height = 100;
         this.speed = Math.random() * 4 -2; //don't understand this line
         this.spriteWidth = 293;
         this.spriteHeight = 155;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.frame = 0;
         ENEMIES_ARRAY.push(this);
     }
 
     update() {
         this.x += this.speed;
         this.y += this.speed;
+        this.frame > 4 ? this.frame = 0 : this.frame++;
     }
 
     draw() {
         ctx.strokeRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(enemyImage1, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx.drawImage(enemyImage1, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 
