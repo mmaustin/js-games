0cc8e9b1dfd8c6d7942d04066e7d76eab25b8d54
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-01 11:39:00 -0400

    removed this speed to prevent sprites from leaving the canvas

--- enemyMovement/script.js
+++ enemyMovement/script.js
@@ -6,34 +6,34 @@ CANVAS_HEIGHT = canvas.height = 1000;
 numberOfEnemies = 100;
 ENEMIES_ARRAY = [];
 
-const enemyImage1 = new Image();
-enemyImage1.src = '../images/enemy1.png';
 let gameFrame = 0;
 
 class Enemy{
     constructor(){
-        this.x = Math.random() * CANVAS_WIDTH;
-        this.y = Math.random() * CANVAS_HEIGHT;
-        this.speed = Math.random() * 4 -2; //don't understand this line
+        this.image = new Image();
+        this.image.src = '../images/enemy1.png'
+        //this.speed = Math.random() * 4 -2; //don't understand this line
         this.spriteWidth = 293;
         this.spriteHeight = 155;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
         ENEMIES_ARRAY.push(this);
     }
 
     update() {
-        this.x += this.speed;
-        this.y += this.speed;
-        if(gameFrame % 2 === 0){
+        this.x += Math.random() * 5 - 2.5;
+        this.y += Math.random() * 5 - 2.5;
+        if(gameFrame % this.flapSpeed === 0){
             this.frame > 4 ? this.frame = 0 : this.frame++;
         }
     }
 
     draw() {
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(enemyImage1, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 
