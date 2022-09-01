c3b1ede70a2151f4bcf283588e7b369377ee3219
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-01 11:46:47 -0400

    switched to second enemy image

--- enemyMovement/script.js
+++ enemyMovement/script.js
@@ -1,8 +1,64 @@
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
+
+
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
+
+numberOfEnemies = 100;
+ENEMIES_ARRAY = [];
+
+let gameFrame = 0;
+
+class Enemy{
+    constructor(){
+        this.image = new Image();
+        this.image.src = '../images/enemy2.png'
+        //this.speed = Math.random() * 4 -2; //don't understand this line
+        this.spriteWidth = 266;
+        this.spriteHeight = 188;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+        ENEMIES_ARRAY.push(this);
+    }
+
+    update() {
+        this.x += Math.random() * 5 - 2.5;
+        this.y += Math.random() * 5 - 2.5;
+        if(gameFrame % this.flapSpeed === 0){
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+}
+
+for(let i = 0; i < numberOfEnemies; i++){
+    new Enemy();
+}
+
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ENEMIES_ARRAY.map(enemy => {
+        enemy.update();
+        enemy.draw();
+    })
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
+animate();
+
+
+
+
+/*  enemies largely stay on the canvas
 numberOfEnemies = 100;
 ENEMIES_ARRAY = [];
 
@@ -50,4 +106,4 @@ function animate(){
     gameFrame++;
     requestAnimationFrame(animate);
 }
-animate();
+animate();*/
