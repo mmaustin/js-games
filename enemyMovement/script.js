e492b3309ce352485a76444294d82bf3cbc1557f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-11 12:00:58 -0400

    added math.cos to update function to exhibit circular rotation of images

--- enemyMovement/script.js
+++ enemyMovement/script.js
@@ -6,7 +6,7 @@ const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
 
-numberOfEnemies = 20;
+numberOfEnemies = 50;
 ENEMIES_ARRAY = [];
 
 let gameFrame = 0;
@@ -14,10 +14,10 @@ let gameFrame = 0;
 class Enemy{
     constructor(){
         this.image = new Image();
-        this.image.src = '../images/enemy2.png'
+        this.image.src = '../images/enemy3.png'
         this.speed = Math.random() * 4 + 1;
-        this.spriteWidth = 266;
-        this.spriteHeight = 188;
+        this.spriteWidth = 218;
+        this.spriteHeight = 177;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
         this.x = Math.random() * (canvas.width - this.width);
@@ -25,15 +25,18 @@ class Enemy{
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
         this.angle = 0;
-        this.angleSpeed = Math.random() * 0.2;
-        this.curve = Math.random() * 6;
+        this.angleSpeed = Math.random() * 2;
+        this.curve = Math.random() * 200;
 
         ENEMIES_ARRAY.push(this);
     }
 
     update() {
-        this.x -= this.speed;
-        this.y += this.curve * Math.sin(this.angle);
+        this.x = this.curve * Math.sin(this.angle * Math.PI/180) + 
+        (canvas.width/2 - this.width);
+        this.y = this.curve * Math.cos(this.angle * Math.PI/180) + 
+        (canvas.height/2 - this.height);
+        //this.y += this.curve * Math.sin(this.angle);
         this.angle += this.angleSpeed;
         if(this.x + this.width < 0) this.x = canvas.width;
         if(gameFrame % this.flapSpeed === 0){
