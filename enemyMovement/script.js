7262e4a5ffca619af182599d8c6a9cb9a9138d0b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-09-11 12:25:10 -0400

    returned animations to original static positions

--- enemyMovement/script.js
+++ enemyMovement/script.js
@@ -24,6 +24,58 @@ class Enemy{
         this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+
+        ENEMIES_ARRAY.push(this);
+    }
+
+    update() {
+        //this.x = 0;
+        //this.y = 0;
+        if(this.x + this.width < 0) this.x = canvas.width;
+        if(gameFrame % this.flapSpeed === 0){
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+}
+
+for(let i = 0; i < numberOfEnemies; i++){
+    new Enemy();
+}
+
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ENEMIES_ARRAY.map(enemy => {
+        enemy.update();
+        enemy.draw();
+    })
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
+animate();
+
+/* Enemy movement patterns
+numberOfEnemies = 50;
+ENEMIES_ARRAY = [];
+
+let gameFrame = 0;
+
+class Enemy{
+    constructor(){
+        this.image = new Image();
+        this.image.src = '../images/enemy3.png'
+        this.speed = Math.random() * 4 + 1;
+        this.spriteWidth = 218;
+        this.spriteHeight = 177;
+        this.width = this.spriteWidth / 2.5;
+        this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
         this.angle = 0;
         this.angleSpeed = Math.random() * 2 + 0.5;
         this.curve = Math.random() * 200 + 5;
@@ -63,8 +115,7 @@ function animate(){
     requestAnimationFrame(animate);
 }
 animate();
-
-
+*/
 
 
 /*  enemies largely stay on the canvas
